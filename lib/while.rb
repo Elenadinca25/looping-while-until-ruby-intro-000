e73283f5ed18ebca294401d5357544d79e861d37
Elena Dinca <elenadinca25@gmail.com>
2020-02-27 00:01:02 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -5,3 +5,11 @@ while levitation_force < 10
   levitation_force += 1
 end
 end
+
+def using_until
+  levitation_force = 6
+  until levitation_force == 10
+    puts "Wingardium Leviosa"
+    levitation_force += 1
+  end  
+  end
